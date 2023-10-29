d4296a6a8f5fe8b4f3f9ec07c82f56f72544f936
LBPosterity <137121988+LBPosterity@users.noreply.github.com>
2023-10-29 11:54:27 +1100

    more

--- scripts.js
+++ scripts.js
@@ -1,62 +1,82 @@
 const listingsData = [
     {
+      hero: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
       name: "Listing Example A",
       description: "my description goes here",
       tagParentA: "Parent Tag A",
       tagChildA: ["Child Tag A", "Child Tag B", "Child Tag C"],
       tagParentB: "Parent Tag B",
       tagChildB: ["Child Tag A", "Child Tag B", "Child Tag C"],
+      examples: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
+    },
+    {
       hero: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
-      examples: "https://learnwebcode.github.io/json-example/images/dog-1.jpg",
+      name: "Listing Example B",
+      description: "my description goes here",
+      tagParentA: "Parent Tag A",
+      tagChildA: ["Child Tag A", "Child Tag B", "Child Tag C"],
+      tagParentB: "Parent Tag B",
+      tagChildB: ["Child Tag A", "Child Tag B", "Child Tag C"],
+      examples: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
     },
     {
-      name: "Barksalot",
-      hero: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
-    },
-    {
-      name: "Meowsalot",
-      tagChildA: ["tuna", "catnip", "sdfsdf"],
-      hero: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
+      hero: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
+      name: "Listing Example C",
+      description: "my description goes here",
+      tagParentA: "Parent Tag A",
+      tagChildA: ["Child Tag A", "Child Tag B", "Child Tag C"],
+      tagParentB: "Parent Tag B",
+      tagChildB: ["Child Tag A", "Child Tag B", "Child Tag C"],
+      examples: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
     }
   ];
   
   
+
   function childTagsA(childTagsA) {
     return `
-      ${childTagsA.map(tags => `<div>${tags}</div>`).join("")}
+      ${childTagsA.map(tags => `<div class="tag">${tags}</div>`).join("")}
     `;
   }
+
 
   function childTagsB(childTagsB) {
     return `
-      ${childTagsB.map(tags => `<div>${tags}</div>`).join("")}
+      ${childTagsB.map(tags => `<div class="tag">${tags}</div>`).join("")}
     `;
   }
+
   
   function listingTemplate(listing) {
     return `
-      <div class="animal">
-        <img class="listing-hero" src="${listing.hero}">
-        <h2 class="listing-name">${listing.name}</h2>
-        ${listing.description ? `<p>${listing.description}</p>` : ""}
+      <div class="listing-wrapper">
+        <div class="header-wrapper">
+          <div class="hero-wrapper">
+            <img class="listing-hero" src="${listing.hero}">
+          </div>
+          <div class="title-wrapper">
+            <h3 class="listing-name">${listing.name}</h3>
+            ${listing.description ? `<p>${listing.description}</p>` : ""}
 
-        ${listing.tagParentA ? `<div>${listing.tagParentA} 
-            ${listing.tagChildA ? childTagsA(listing.tagChildA) : ""} 
-        </div>` : ""}
+            ${listing.tagParentA ? `<div class="tag parent">${listing.tagParentA} 
+                ${listing.tagChildA ? childTagsA(listing.tagChildA) : ""} 
+            </div>` : ""}
 
-        ${listing.tagParentB ? `<div>${listing.tagParentB} 
-          ${listing.tagChildB ? childTagsB(listing.tagChildB) : ""} 
-        </div>` : ""}
+            ${listing.tagParentB ? `<div class="tag parent">${listing.tagParentB} 
+              ${listing.tagChildB ? childTagsB(listing.tagChildB) : ""} 
+            </div>` : ""}
+          </div>
+        </div>
 
-        ${ listing.examples ? `<img class="listing-hero" src="${listing.examples}">` : ""}
+        <div class="details-wrapper">
+          <h4>Details</h4>
+          ${ listing.examples ? `<img class="listing-hero" src="${listing.examples}">` : ""}
+        </div>
       
       </div>
     `;
   }
   
-  document.getElementById("app").innerHTML = `
-    <h1 class="app-title">listings (${listingsData.length} results)</h1>
-    ${listingsData.map(listingTemplate).join("")}
-    <p class="footer">These ${listingsData.length} listings were added recently. Check back soon for updates.</p>
-  `;
+
+  document.getElementById("app").innerHTML = `${listingsData.map(listingTemplate).join("")}`;
   
